e50fb25c94598a23411484cb13b100cf2c2ed083
BrigtHaavardstun <Brigt_Arve@outlook.com>
2024-06-07 16:42:42 +0200

    [🔧|FullLinePerturbation] Add compute and vizulation

--- Perturbations/fullLinePerturbation.py
+++ Perturbations/fullLinePerturbation.py
@@ -1,40 +1,108 @@
-from random import random
+import random
+from typing import List
+import numpy as np
+
+from matplotlib.colors import to_rgba
 
 from Perturbations.dataTypes import PerturbationTS
+from models.loadModel import model_batch_classify
 
+from utils.model import class_to_color
+from utils.data import get_time_series_and_seg_pivots
+from utils.line import interpolate_points_to_line
 
-def create_permutations(points_y, max_y, min_y, k=10 ** 6, e=0.08):
-    dataset_dist = abs(max_y - min_y)
-    epsilon = e
-
-    change_range = dataset_dist * epsilon
-    permutations = []
-    for i in range(k):
-        new_val_points = []
-        for point in points_y:
-            random_val = random.gauss(0, 1)  # Currently using gaussion random, could use uniform random.uniform(-1, 1))
-
-            new_val_points.append(point + change_range * random_val)
-        permutations.append(new_val_points)
-
-    return permutations
+from visualization.plotting import TSParam, PlotParams
 
 
 def create_x_y_perturbations(org_pivots_y: List[float], org_pivots_x: List[int], ts_length: int, epsilon: float) -> \
         List[PerturbationTS]:
-    resolution = 10 ** 4  # Number of lines
+    all_perturbations = []
+    resolution = int(5.0e4)  # Number of lines
     for i in range(resolution):
         new_pivots_y = []
         new_pivots_x = []
         for j in range(len(org_pivots_x)):
-            random_y_change = random.gauss(0, epsilon)
-            x_range = 1
-            possible_x_values = list(range(-x_range, x_range + 1))
-            random_x_change = random.choice(possible_x_values)
-
+            # random_y_change = random.gauss(0, epsilon) Gaussion
+            random_y_change = random.uniform(-epsilon, epsilon)
             new_pivot_y = org_pivots_y[j] + random_y_change
+
+            x_range = 0
+            possible_x_values = list(range(-x_range, x_range + 1))
+            if j == 0:
+                possible_x_values = list(range(0, x_range + 1))
+            if j == len(org_pivots_x) - 1:
+                possible_x_values = list(range(-x_range, 1))
+            random_x_change = random.choice(possible_x_values)
             new_pivot_x = org_pivots_x[j] + random_x_change
+            count = 1000
+            while new_pivot_x in new_pivots_x:  # Ensure that we don't get an impossible line
+                count -= 1
+                print("In the loooop")
+                random_x_change = random.choice(possible_x_values)
+                new_pivot_x = org_pivots_x[j] + random_x_change
+                if count <= 0:
+                    raise Exception("Stuck", new_pivot_x, possible_x_values, org_pivots_x)
+
             new_pivots_y.append(new_pivot_y)
             new_pivots_x.append(new_pivot_x)
+        tsParam = PerturbationTS(x_pivots=new_pivots_x, y_pivots=new_pivots_y)
+        tsParam.set_line_version(ts_length)
+        all_perturbations.append(tsParam)
+    return all_perturbations
 
-    return []
+
+def classify_all_perturbations(all_perturbations: List[PerturbationTS], model_name: str) -> List[PerturbationTS]:
+    all_line_version = [perturbation.line_version for perturbation in all_perturbations]
+    all_pred_classes = model_batch_classify(batch_of_timeseries=all_line_version, model_name=model_name)
+    for perturbation, pred_class in zip(all_perturbations, all_pred_classes):  # type: PerturbationTS, int
+        perturbation.set_class(pred_class)
+
+    return all_perturbations
+
+
+def full_perturbations_to_ts_params(all_perturbations: List[PerturbationTS]) -> List[TSParam]:
+    all_perturbation_params = []
+    for perturbation in all_perturbations:
+        alpha = 2e-3
+        ts_param = TSParam(
+            x_values=list(range(0, len(perturbation.line_version))),
+            y_values=perturbation.line_version,
+            color=to_rgba(
+                class_to_color(perturbation.pred_class),
+                alpha=alpha
+            )
+        )
+        all_perturbation_params.append(ts_param)
+    return all_perturbation_params
+
+
+def plot_full_line_perturbations(all_perturbations: List[PerturbationTS], original_ts: List[float] | np.ndarray,
+                                 approximation_ts: List[float], model_name: str, pivot_x_org: List[int],
+                                 pivot_y_org: List[float],
+                                 pivot_x_approx: List[int], pivot_y_approx: List[int]):
+    all_perturbation_params = full_perturbations_to_ts_params(all_perturbations)
+    PlotParams(ts_params=all_perturbation_params, display=True).make_plot()
+
+
+def run(dataset_name, model_name, instance_nr):
+    pivot_points_x, pivot_points_y, ts, min_y, max_y = get_time_series_and_seg_pivots(dataset_name=dataset_name,
+                                                                                      instance_nr=instance_nr)
+    approximation_line = interpolate_points_to_line(len(ts), pivot_points_x, pivot_points_y)
+    epsilon = (max_y - min_y) / 10
+    perturbations = create_x_y_perturbations(org_pivots_y=pivot_points_y, org_pivots_x=pivot_points_x,
+                                             ts_length=len(ts), epsilon=epsilon)
+    perturbations = classify_all_perturbations(all_perturbations=perturbations, model_name=model_name)
+    plot_full_line_perturbations(all_perturbations=perturbations, original_ts=ts, approximation_ts=approximation_line,
+                                 model_name=model_name, pivot_x_org=pivot_points_x, pivot_y_org=pivot_points_y,
+                                 pivot_x_approx=pivot_points_x, pivot_y_approx=pivot_points_y)
+
+
+def start_run():
+    model_name = "Chinatown_1000.keras"
+    dataset_name = "Chinatown"
+    for i in range(0, 100, 1):
+        run(instance_nr=i, model_name=model_name, dataset_name=dataset_name)
+
+
+if __name__ == '__main__':
+    start_run()
